79aa512ec5b2b2c9ea9a89ca171b198a292a2b5d
OPoleshchuk <oleksii.poleshchuk@gmail.com>
2019-05-31 17:13:05 +0200

    it requires input of one name instead of three in the .h file

--- AddBack.c
+++ AddBack.c
@@ -7,114 +7,114 @@
 
 void AddBack::Loop()
 {
-//   In a ROOT session, you can do:
-//      Root > .L AddBack.C
-//      Root > AddBack t
-//      Root > t.GetEntry(12); // Fill t data members with entry number 12
-//      Root > t.Show();       // Show values of entry 12
-//      Root > t.Show(16);     // Read and show values of entry 16
-//      Root > t.Loop();       // Loop on all entries
-//
+    //   In a ROOT session, you can do:
+    //      Root > .L AddBack.C
+    //      Root > AddBack t
+    //      Root > t.GetEntry(12); // Fill t data members with entry number 12
+    //      Root > t.Show();       // Show values of entry 12
+    //      Root > t.Show(16);     // Read and show values of entry 16
+    //      Root > t.Loop();       // Loop on all entries
+    //
 
-//     This is the loop skeleton where:
-//    jentry is the global entry number in the chain
-//    ientry is the entry number in the current Tree
-//  Note that the argument to GetEntry must be:
-//    jentry for TChain::GetEntry
-//    ientry for TTree::GetEntry and TBranch::GetEntry
-//
-//       To read only selected branches, Insert statements like:
-// METHOD1:
-//    fChain->SetBranchStatus("*",0);  // disable all branches
-//    fChain->SetBranchStatus("branchname",1);  // activate branchname
-// METHOD2: replace line
-//    fChain->GetEntry(jentry);       //read all branches
-//by  b_branchname->GetEntry(ientry); //read only this branch
-   if (fChain == 0) return;
+    //     This is the loop skeleton where:
+    //    jentry is the global entry number in the chain
+    //    ientry is the entry number in the current Tree
+    //  Note that the argument to GetEntry must be:
+    //    jentry for TChain::GetEntry
+    //    ientry for TTree::GetEntry and TBranch::GetEntry
+    //
+    //       To read only selected branches, Insert statements like:
+    // METHOD1:
+    //    fChain->SetBranchStatus("*",0);  // disable all branches
+    //    fChain->SetBranchStatus("branchname",1);  // activate branchname
+    // METHOD2: replace line
+    //    fChain->GetEntry(jentry);       //read all branches
+    //by  b_branchname->GetEntry(ientry); //read only this branch
+    if (fChain == 0) return;
 
-   Long64_t nentries = fChain->GetEntriesFast();
+    Long64_t nentries = fChain->GetEntriesFast();
 
-   Double_t EventPrev = 0;
-   Double_t CrystNbPrev = 0;
-   Double_t EdepPrev = 0;
-   Double_t EdepSum = 0;
-   Double_t EdepAddBackArray[3000000];
+    Double_t EventPrev = 0;
+    Double_t CrystNbPrev = 0;
+    Double_t EdepResPrev = 0;
+    Double_t EdepResSum = 0;
+    Double_t EdepResAddBackArray[3000000];
 
-   static const Int_t NbCrystInRing = 24;
-   static const Int_t NbRings = 3;
+    static const Int_t NbCrystInRing = 15;
+    static const Int_t NbRings = 2;
 
-   Long64_t nbytes = 0, nb = 0;
-   Long64_t itr = 0;
+    Long64_t nbytes = 0, nb = 0;
+    Long64_t itr = 0;
 
+    Int_t counter = 1;
+    Int_t CrystArray[NbCrystInRing][NbRings];
+    for (Int_t i=0; i<NbCrystInRing; i++) {
+        for (Int_t j=0; j<NbRings; j++) {
+            CrystArray[i][j] = counter;
+            counter += 1;
+        }
+    }
 
-   Int_t counter = 1;
-   Int_t CrystArray[NbCrystInRing][NbRings];
-   for (Int_t i=0; i<NbCrystInRing; i++) {
-      for (Int_t j=0; j<NbRings; j++) {
-         CrystArray[i][j] = counter;
-	 counter += 1;
-      }
-   }
+    Int_t PositionI1, PositionI2, PositionJ1, PositionJ2;
 
-   Int_t PositionI1, PositionI2, PositionJ1, PositionJ2;
+    for (Long64_t jentry=0; jentry<nentries;jentry++) {
+        Long64_t ientry = LoadTree(jentry);
+        if (ientry < 0) break;
+        nb = fChain->GetEntry(jentry);   nbytes += nb;
+        // if (Cut(ientry) < 0) continue;
+        itr += 1;
+        if (Event == EventPrev) {
 
-   for (Long64_t jentry=0; jentry<nentries;jentry++) {
-      Long64_t ientry = LoadTree(jentry);
-      if (ientry < 0) break;
-      nb = fChain->GetEntry(jentry);   nbytes += nb;
-      // if (Cut(ientry) < 0) continue;
-      itr += 1;
-      if (Event == EventPrev) {
+            for (Int_t i=0; i<NbCrystInRing; i++) {
+                for (Int_t j=0; j<NbRings; j++) {
+                    if (CrystArray[i][j] == CrystNb) {
+                        PositionI1 = i;
+                        PositionJ1 = j;
+                    }
+                    if (CrystArray[i][j] == CrystNbPrev) {
+                        PositionI2 = i;
+                        PositionJ2 = j;
+                    }
+                }
+            }
+            if ((fabs(PositionI1-PositionI2)<=1) && (fabs(PositionJ1-PositionJ2)<=1)) {
+                EdepResSum = EdepRes + EdepResPrev;
+                itr -= 1;
+            }
+            else {
+                EdepResSum = EdepRes;
+            }
+        }
 
-   	for (Int_t i=0; i<NbCrystInRing; i++) {
-      	   for (Int_t j=0; j<NbRings; j++) {
-              if (CrystArray[i][j] == CrystNb) {
-	         PositionI1 = i;
-		 PositionJ1 = j;
-	      }
-              if (CrystArray[i][j] == CrystNbPrev) {
-	         PositionI2 = i;
-		 PositionJ2 = j;
-	      }
+        else {
+            EdepResSum = EdepRes;
+        }
 
-	   }
-	}
-	if ((fabs(PositionI1-PositionI2)<=1) && (fabs(PositionJ1-PositionJ2)<=1)) {
-	   EdepSum = Edep + EdepPrev;
-           itr -= 1;
-	}
-	else {
-	   EdepSum = Edep;
-	}
-      }
+        EdepResAddBackArray[itr] = EdepResSum;
+        EventPrev = Event;
+        CrystNbPrev = CrystNb;
+        EdepResPrev = EdepRes;
+        /*
+        if (Event%10000 == 0) {
+          cout<<"Event "<<Event<<"; "<<"CrystNb "<<CrystNb<<"; "<<"EdepRes "<<EdepRes<<"; "<<EdepResSum<<endl;
+          cout<<EdepResAddBackArray[itr]<<" length ="<<itr<<endl;
+        }*/
 
-      else {
-         EdepSum = Edep;
-      }
+    }
 
-      EdepAddBackArray[itr] = EdepSum;
-      EventPrev = Event;
-      CrystNbPrev = CrystNb;
-      EdepPrev = Edep;
+    TFile *File = new TFile(Form("AddBack_%s", fileN.c_str()),"recreate");
+    TTree *Tree = new TTree("Total","Total EdepRes after AddBack");
+    TH1F *h1 = new TH1F("TotalHistAB","Total EdepRes after AddBack", 1500, 0., 1500);
+    Double_t EdepResAddBack;
+    Tree->Branch("EdepNoRes", &EdepResAddBack, "EdepResAddBack/D");
 
-      cout<<"Event "<<Event<<"; "<<"CrystNb "<<CrystNb<<"; "<<"Edep "<<Edep<<"; "<<EdepSum<<endl;
-      cout<<EdepAddBackArray[itr]<<" length ="<<itr<<endl;
+    for (Int_t ij=0; ij<itr; ij++) {
+        EdepResAddBack=EdepResAddBackArray[ij];
+        Tree->Fill();
+        h1->Fill(EdepResAddBack);
+    }
 
-   }
-
-   TFile *File = new TFile("AddBack.root","recreate");
-   TTree *Tree = new TTree("Total","Total Edep after AddBack");
-   TH1F *h1 = new TH1F("TotalHist","Total Edep after AddBack", 15501, 0., 15500);
-   Double_t EdepAddBack;
-   Tree->Branch("Edep", &EdepAddBack, "EdepAddBack/D");
-
-   for (Int_t ij=0; ij<itr; ij++) {
-	 EdepAddBack=EdepAddBackArray[ij];
-	 Tree->Fill();
-     h1->Fill(EdepAddBack);
-   }
-
-   Tree->Write();
-   h1->Write();
-   File->Close();
+    Tree->Write();
+    h1->Write();
+    File->Close();
 }
--- AddBack.h
+++ AddBack.h
@@ -1,8 +1,8 @@
 //////////////////////////////////////////////////////////
 // This class has been automatically generated on
 // Thu Mar 24 14:27:40 2016 by ROOT version 5.34/34
-// from TTree Total/Total Edep
-// found on file: CeBr3_38mmx38mmx38mm_16x1x1crystals_R115.629mm_2.5MeV.root
+// from TTree Total/Total EdepRes
+//
 //////////////////////////////////////////////////////////
 
 #ifndef AddBack_h
@@ -13,33 +13,34 @@
 #include <TFile.h>
 
 // Header file for the classes stored in the TTree if any.
-
 // Fixed size dimensions of array or collections stored in the TTree if any.
 
 class AddBack {
-public :
-   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
-   Int_t           fCurrent; //!current Tree number in a TChain
+  public :
+  TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+  Int_t           fCurrent; //!current Tree number in a TChain
 
-   // Declaration of leaf types
-   Double_t        Event;
-   Double_t        CrystNb;
-   Double_t        Edep;
+  // Declaration of leaf types
+  Double_t        Event;
+  Double_t        CrystNb;
+  Double_t        EdepRes;
 
-   // List of branches
-   TBranch        *b_Event;   //!
-   TBranch        *b_CrystNb;   //!
-   TBranch        *b_Edep;   //!
+  string fileN;
 
-   AddBack(TTree *tree=0);
-   virtual ~AddBack();
-   virtual Int_t    Cut(Long64_t entry);
-   virtual Int_t    GetEntry(Long64_t entry);
-   virtual Long64_t LoadTree(Long64_t entry);
-   virtual void     Init(TTree *tree);
-   virtual void     Loop();
-   virtual Bool_t   Notify();
-   virtual void     Show(Long64_t entry = -1);
+  // List of branches
+  TBranch        *b_Event;   //!
+  TBranch        *b_CrystNb;   //!
+  TBranch        *b_EdepRes;   //!
+
+  AddBack(TTree *tree=0);
+  virtual ~AddBack();
+  virtual Int_t    Cut(Long64_t entry);
+  virtual Int_t    GetEntry(Long64_t entry);
+  virtual Long64_t LoadTree(Long64_t entry);
+  virtual void     Init(TTree *tree);
+  virtual void     Loop();
+  virtual Bool_t   Notify();
+  virtual void     Show(Long64_t entry = -1);
 
 };
 
@@ -48,91 +49,93 @@ public :
 #ifdef AddBack_cxx
 AddBack::AddBack(TTree *tree) : fChain(0)
 {
-// if parameter tree is not specified (or zero), connect the file
-// used to generate this class and read the Tree.
-   if (tree == 0) {
-      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("CeBr3_38mmx38mmx38mm_6x4x3crystals_R159.349mm_Cs137[0.0]MeV_3000000.root");
-      if (!f || !f->IsOpen()) {
-         f = new TFile("CeBr3_38mmx38mmx38mm_6x4x3crystals_R159.349mm_Cs137[0.0]MeV_3000000.root");
-      }
-      TDirectory * dir = (TDirectory*)f->Get("CeBr3_38mmx38mmx38mm_6x4x3crystals_R159.349mm_Cs137[0.0]MeV_3000000.root:/ntuple");
-      dir->GetObject("Total",tree);
+  // if parameter tree is not specified (or zero), connect the file
+  // used to generate this class and read the Tree.
 
-   }
-   Init(tree);
+  fileN = "CeBr3_48mmx48mmx48mm_15x1x2crystals_R131.25mm_gamma11MeV_Al_Chamber_pos2_pointSource_gap_3.root";
+  if (tree == 0) {
+    TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject(fileN.c_str());
+    if (!f || !f->IsOpen()) {
+      f = new TFile(fileN.c_str());
+    }
+    TDirectory * dir = (TDirectory*)f->Get(Form("%s:/ntuple", fileN.c_str()));
+    dir->GetObject("Total",tree);
+
+  }
+  Init(tree);
 
 }
 
 AddBack::~AddBack()
 {
-   if (!fChain) return;
-   delete fChain->GetCurrentFile();
+  if (!fChain) return;
+  delete fChain->GetCurrentFile();
 }
 
 Int_t AddBack::GetEntry(Long64_t entry)
 {
-// Read contents of entry.
-   if (!fChain) return 0;
-   return fChain->GetEntry(entry);
+  // Read contents of entry.
+  if (!fChain) return 0;
+  return fChain->GetEntry(entry);
 }
 Long64_t AddBack::LoadTree(Long64_t entry)
 {
-// Set the environment to read one entry
-   if (!fChain) return -5;
-   Long64_t centry = fChain->LoadTree(entry);
-   if (centry < 0) return centry;
-   if (fChain->GetTreeNumber() != fCurrent) {
-      fCurrent = fChain->GetTreeNumber();
-      Notify();
-   }
-   return centry;
+  // Set the environment to read one entry
+  if (!fChain) return -5;
+  Long64_t centry = fChain->LoadTree(entry);
+  if (centry < 0) return centry;
+  if (fChain->GetTreeNumber() != fCurrent) {
+    fCurrent = fChain->GetTreeNumber();
+    Notify();
+  }
+  return centry;
 }
 
 void AddBack::Init(TTree *tree)
 {
-   // The Init() function is called when the selector needs to initialize
-   // a new tree or chain. Typically here the branch addresses and branch
-   // pointers of the tree will be set.
-   // It is normally not necessary to make changes to the generated
-   // code, but the routine can be extended by the user if needed.
-   // Init() will be called many times when running on PROOF
-   // (once per file to be processed).
+  // The Init() function is called when the selector needs to initialize
+  // a new tree or chain. Typically here the branch addresses and branch
+  // pointers of the tree will be set.
+  // It is normally not necessary to make changes to the generated
+  // code, but the routine can be extended by the user if needed.
+  // Init() will be called many times when running on PROOF
+  // (once per file to be processed).
 
-   // Set branch addresses and branch pointers
-   if (!tree) return;
-   fChain = tree;
-   fCurrent = -1;
-   fChain->SetMakeClass(1);
+  // Set branch addresses and branch pointers
+  if (!tree) return;
+  fChain = tree;
+  fCurrent = -1;
+  fChain->SetMakeClass(1);
 
-   fChain->SetBranchAddress("Event", &Event, &b_Event);
-   fChain->SetBranchAddress("CrystNb", &CrystNb, &b_CrystNb);
-   fChain->SetBranchAddress("Edep", &Edep, &b_Edep);
-   Notify();
+  fChain->SetBranchAddress("Event", &Event, &b_Event);
+  fChain->SetBranchAddress("CrystNb", &CrystNb, &b_CrystNb);
+  fChain->SetBranchAddress("EdepNoRes", &EdepRes, &b_EdepRes);
+  Notify();
 }
 
 Bool_t AddBack::Notify()
 {
-   // The Notify() function is called when a new file is opened. This
-   // can be either for a new TTree in a TChain or when when a new TTree
-   // is started when using PROOF. It is normally not necessary to make changes
-   // to the generated code, but the routine can be extended by the
-   // user if needed. The return value is currently not used.
+  // The Notify() function is called when a new file is opened. This
+  // can be either for a new TTree in a TChain or when when a new TTree
+  // is started when using PROOF. It is normally not necessary to make changes
+  // to the generated code, but the routine can be extended by the
+  // user if needed. The return value is currently not used.
 
-   return kTRUE;
+  return kTRUE;
 }
 
 void AddBack::Show(Long64_t entry)
 {
-// Print contents of entry.
-// If entry is not specified, print current entry
-   if (!fChain) return;
-   fChain->Show(entry);
+  // Print contents of entry.
+  // If entry is not specified, print current entry
+  if (!fChain) return;
+  fChain->Show(entry);
 }
 Int_t AddBack::Cut(Long64_t entry)
 {
-// This function may be called from Loop.
-// returns  1 if entry is accepted.
-// returns -1 otherwise.
-   return 1;
+  // This function may be called from Loop.
+  // returns  1 if entry is accepted.
+  // returns -1 otherwise.
+  return 1;
 }
 #endif // #ifdef AddBack_cxx
